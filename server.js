a1729e0a252ed8be9bd7f4052eabc26884f45a6c
shiv7700 <shivangramola007@gmail.com>
2024-10-14 12:31:16 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, "dist")));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "dist/index.html"));
+});
+
+app.get("/api/hello", (req, res) => {
+  res.json({ message: "Hello from Express!" });
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
